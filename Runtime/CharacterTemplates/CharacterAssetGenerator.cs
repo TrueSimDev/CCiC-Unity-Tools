14a380324f65a4bb682d641afcd231199c525adb
Yassine Minjon <yassine@truesim.com>
2026-09-23 13:12:30 +0200

    fix: make CharacterAssetGenerator serialized scriptable object

--- Runtime/CharacterTemplates/CharacterAssetGenerator.cs
+++ Runtime/CharacterTemplates/CharacterAssetGenerator.cs
@@ -1,9 +1,10 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using static UnityEditor.PrefabUtility;
 
 namespace TrueSim.Runtime.CCiC.CharacterTemplates
 {
-    public abstract class CharacterAssetGenerator : ScriptableObject
+    public abstract class CharacterAssetGenerator : SerializedScriptableObject
     {
         public abstract void Apply(EditPrefabContentsScope contentScope, string characterName, string characterAssetPath);
     }
